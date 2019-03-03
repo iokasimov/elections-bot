a692f62ffe4e6c00261be473e596fa8961b2d267
Murat Kasimov <iokasimov.m@gmail.com>
2019-03-03 19:40:48 +0300

    Sketch up executable module

--- Mafia/Bot.hs
+++ Mafia/Bot.hs
@@ -1,5 +1,11 @@
-import Options.Applicative (Parser, argument, metavar, str)
-import Web.Telegram.API.Bot.API (Token (..))
+import Control.Monad.IO.Class (liftIO)
+import Options.Applicative (Parser, execParser, argument, info, fullDesc, metavar, str)
+import Network.HTTP.Client (Manager, newManager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.Wai.Handler.Warp (run)
+import Servant (Capture, ReqBody, Proxy (Proxy), Server, JSON, Get, Post, type (:>), serve, err403, throwError)
+import Web.Telegram.API.Bot.API (Token (Token))
+import Web.Telegram.API.Bot.Data (Update (..))
 
 import qualified Data.Text as T
 
@@ -9,4 +15,13 @@ options :: Parser Options
 options = fmap Options $ fmap (Token . T.pack) $
 	argument str $ metavar "TOKEN"
 
-main = print "typechecked"
+type API = "webhook" :> Capture "secret" Token :> ReqBody '[JSON] Update :> Post '[JSON] ()
+
+server :: Token -> Server API
+server _ _ _ = liftIO $ print "REACHED!"
+
+main = do
+	Options token <- execParser $ info options fullDesc
+	manager <- newManager tlsManagerSettings
+	run 8080 (serve api $ server token)
+		where api = Proxy :: Proxy API
