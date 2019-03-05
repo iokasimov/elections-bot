52cf3d64908095df1fbacf29eb8a838eeca12114
Murat Kasimov <iokasimov.m@gmail.com>
2019-03-05 23:49:31 +0300

    Add chat ID CLI argument option

--- Mafia/Bot.hs
+++ Mafia/Bot.hs
@@ -1,27 +1,37 @@
+module Main where
+
+import Control.Monad (void)
 import Control.Monad.IO.Class (liftIO)
-import Options.Applicative (Parser, execParser, argument, info, fullDesc, metavar, str)
+import Options.Applicative (Parser, execParser, argument, auto, info, fullDesc, metavar, str)
 import Network.HTTP.Client (Manager, newManager)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Network.Wai.Handler.Warp (run)
 import Servant (Capture, ReqBody, Proxy (Proxy), Server, JSON, Get, Post, type (:>), serve, err403, throwError)
-import Web.Telegram.API.Bot.API (Token (Token))
-import Web.Telegram.API.Bot.Data (Update (..))
+import Web.Telegram.API.Bot.API (Token (Token), sendMessage)
+import Web.Telegram.API.Bot.Data (Chat (..), Message (..), Update (..))
+import Web.Telegram.API.Bot.Requests (ChatId (ChatId), SendMessageRequest (..), ReplyKeyboard (ReplyKeyboardRemove))
 
 import qualified Data.Text as T
 
-data Options = Options Token deriving Show
+data Options = Options Token ChatId deriving Show
 
 options :: Parser Options
-options = fmap Options $ fmap (Token . T.pack) $
-	argument str $ metavar "TOKEN"
+options = Options
+	<$> (Token . T.pack <$> argument str (metavar "TOKEN"))
+	<*> (ChatId <$> argument auto (metavar "CHATID"))
 
 type API = "webhook" :> Capture "secret" Token :> ReqBody '[JSON] Update :> Post '[JSON] ()
 
-server :: Token -> Server API
-server _ _ _ = liftIO $ print "REACHED!"
+server :: Manager -> Options -> Server API
+server manager opts@(Options token _) secret update = if secret == token
+	then liftIO $ webhook manager opts update
+	else throwError err403
+
+webhook :: Manager -> Options -> Update -> IO ()
+webhook _ _ _ = pure ()
 
 main = do
-	Options token <- execParser $ info options fullDesc
+	opts <- execParser $ info options fullDesc
 	manager <- newManager tlsManagerSettings
-	run 8080 (serve api $ server token)
+	run 8080 (serve api $ server manager opts)
 		where api = Proxy :: Proxy API
