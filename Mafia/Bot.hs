b4b6348b36ebb0bc3644a69dcdc4f228cf85a0c0
Murat Kasimov <iokasimov.m@gmail.com>
2019-03-06 12:50:39 +0300

    Create Settings datatype to use it as enviroenment value

--- Mafia/Bot.hs
+++ Mafia/Bot.hs
@@ -1,37 +1,38 @@
 module Main where
 
-import Control.Monad (void)
+import Control.Concurrent.STM (TVar, newTVarIO)
 import Control.Monad.IO.Class (liftIO)
 import Options.Applicative (Parser, execParser, argument, auto, info, fullDesc, metavar, str)
 import Network.HTTP.Client (Manager, newManager)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Network.Wai.Handler.Warp (run)
 import Servant (Capture, ReqBody, Proxy (Proxy), Server, JSON, Get, Post, type (:>), serve, err403, throwError)
-import Web.Telegram.API.Bot.API (Token (Token), sendMessage)
+import Web.Telegram.API.Bot.API (Token (Token))
 import Web.Telegram.API.Bot.Data (Chat (..), Message (..), Update (..))
-import Web.Telegram.API.Bot.Requests (ChatId (ChatId), SendMessageRequest (..), ReplyKeyboard (ReplyKeyboardRemove))
+import Web.Telegram.API.Bot.Requests (ChatId (ChatId))
 
 import qualified Data.Text as T
 
-data Options = Options Token ChatId deriving Show
+data Arguments = Arguments Token ChatId deriving Show
 
-options :: Parser Options
-options = Options
+options :: Parser Arguments
+options = Arguments
 	<$> (Token . T.pack <$> argument str (metavar "TOKEN"))
-	<*> (ChatId <$> argument auto (metavar "CHATID"))
+	<*> (ChatId . negate <$> argument auto (metavar "CHATID"))
+
+data Settings = Settings Token ChatId Manager (TVar [(Int, Int)])
 
 type API = "webhook" :> Capture "secret" Token :> ReqBody '[JSON] Update :> Post '[JSON] ()
 
-server :: Manager -> Options -> Server API
-server manager opts@(Options token _) secret update = if secret == token
-	then liftIO $ webhook manager opts update
+server :: Settings -> Server API
+server settings@(Settings token _ _ _) secret update = if secret == token
+	then liftIO $ webhook settings update
 	else throwError err403
 
-webhook :: Manager -> Options -> Update -> IO ()
-webhook _ _ _ = pure ()
+webhook :: Settings -> Update -> IO ()
+webhook _ _ = pure ()
 
 main = do
-	opts <- execParser $ info options fullDesc
-	manager <- newManager tlsManagerSettings
-	run 8080 (serve api $ server manager opts)
-		where api = Proxy :: Proxy API
+	Arguments token chaid <- execParser $ info options fullDesc
+	settings <- Settings token chaid <$> newManager tlsManagerSettings <*> newTVarIO []
+	run 8080 . serve (Proxy :: Proxy API) $ server settings
