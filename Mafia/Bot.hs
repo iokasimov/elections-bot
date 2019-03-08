f737cc863a306282c85311ab86fc48c61a680f93
Murat Kasimov <iokasimov.m@gmail.com>
2019-03-08 14:14:41 +0300

    Send keyboards to the chat with buttons on candidates

--- Mafia/Bot.hs
+++ Mafia/Bot.hs
@@ -4,13 +4,13 @@ import Control.Monad.IO.Class (liftIO)
 import Network.Wai.Handler.Warp (run)
 import Servant (Capture, ReqBody, Proxy (Proxy), Server, JSON, Get, Post, type (:>), serve, err403, throwError)
 import Web.Telegram.API.Bot.API (Token (Token))
-import Web.Telegram.API.Bot.Data (Chat (..), Message (..), Update (..))
+import Web.Telegram.API.Bot.Data (CallbackQuery (..), Chat (..), Message (..), Update (..))
 import Web.Telegram.API.Bot.Requests (ChatId (ChatId))
 
 import qualified Data.Text as T (take)
 
 import Mafia.Configuration (Settings (Settings), settings)
-import Mafia.Voting (vote)
+import Mafia.Voting (primaries)
 
 type API = "webhook" :> Capture "secret" Token :> ReqBody '[JSON] Update :> Post '[JSON] ()
 
@@ -19,8 +19,9 @@ server settings@(Settings token _ _ _) secret update = if secret == token
 	then liftIO $ webhook settings update else throwError err403
 
 webhook :: Settings -> Update -> IO ()
-webhook settings u@(Update { message = Just (Message { text = Just txt, entities = Just es, chat = Chat { chat_id = cid } }) }) =
-	if T.take 5 txt == "/vote" then vote settings (ChatId cid) es else pure ()
-webhook _ _ = pure ()
+webhook settings@(Settings _ (ChatId cid') _ _)
+	u@(Update { message = Just (Message { text = Just txt, entities = Just es, chat = Chat { chat_id = cid } }) }) =
+		if T.take 5 txt == "/vote" && cid' == cid then primaries settings (ChatId cid) es else pure ()
+webhook _ u = print u
 
 main = settings >>= run 8080 . serve (Proxy :: Proxy API) . server
--- Mafia/Voting.hs
+++ Mafia/Voting.hs
@@ -1,13 +1,14 @@
-module Mafia.Voting (vote) where
+module Mafia.Voting (primaries, vote) where
 
 import Control.Monad (void)
 import Control.Concurrent (threadDelay)
 import Control.Concurrent.STM (atomically, modifyTVar', readTVar)
+import Data.Function ((&))
 import Data.Semigroup (Semigroup ((<>)))
 import Data.Text (Text, pack)
-import Web.Telegram.API.Bot.Data (Chat (..), Message (..), MessageEntity (..), User (..))
+import Web.Telegram.API.Bot.Data (Chat (..), Message (..), MessageEntity (..), User (..), InlineKeyboardButton (..))
 import Web.Telegram.API.Bot.API.Messages (sendMessage)
-import Web.Telegram.API.Bot.Requests (ChatId (ChatId), SendMessageRequest, sendMessageRequest)
+import Web.Telegram.API.Bot.Requests (ChatId (ChatId), ReplyKeyboard (..), SendMessageRequest (SendMessageRequest), sendMessageRequest)
 
 import Mafia.Configuration (Settings (Settings))
 
@@ -17,36 +18,46 @@ import Mafia.Configuration (Settings (Settings))
 -- 2) Actual voting: members of the group chat can send vote command to bot directly,
 --    bot should return the keyboard with members of this chat
 
-vote :: Settings -> ChatId -> [MessageEntity] -> IO ()
-vote settings@(Settings token (ChatId chatid) _ votes) (ChatId chatid') es =
-	if chatid == chatid' -- Voting command in group starts voting
-		then background settings . foldr (\x acc -> maybe acc (flip (:) acc) x) [] . map me_user $ es
-		else undefined -- TODO: When every member is voting (personal message to the bot)
+primaries :: Settings -> ChatId -> [MessageEntity] -> IO ()
+primaries settings@(Settings token (ChatId chatid) _ votes) (ChatId chatid') es =
+	if chatid /= chatid' then pure () else -- Voting command in group starts voting
+		background settings . foldr (\x acc -> maybe acc (flip (:) acc) x) [] . map me_user $ es
+
+background :: Settings -> [User] -> IO ()
+background (Settings token chatid manager votes) us = atomically (readTVar votes) >>= \case
+	Just _ -> void $ sendMessage token taking_place_massage manager
+	Nothing -> do
+		atomically $ modifyTVar' votes (const . Just . map (flip (,) 0) $ us)
+		void $ sendMessage token starting_message manager
+		-- threadDelay 300000000 -- wait for 5 minutes
+		decisions <- atomically (readTVar votes)
+		decisions & maybe (print "Very strange situation")
+			(void . flip (sendMessage token) manager . results chatid)
+		atomically $ modifyTVar' votes (const Nothing)
 	where
-	-- TODO: Send every member keyboard with checkboxes on other members
-	background :: Settings -> [User] -> IO ()
-	background (Settings token chatid manager votes) us =  atomically (readTVar votes) >>= \case
-		Just _ -> void $ sendMessage token taking_place_massage manager
-		Nothing -> do
-			atomically $ modifyTVar' votes (const . Just . map (flip (,) 0) $ us)
-			void $ sendMessage token starting_message manager
-			threadDelay 300000000 -- wait for 5 minutes
-			decisions <- atomically (readTVar votes)
-			decisions & maybe (print "Very strange situation")
-				(void . flip (sendMessage token) manager . results)
-			atomically $ modifyTVar' votes (const Nothing)
 
 	starting_message, taking_place_massage :: SendMessageRequest
-	starting_message = sendMessageRequest (ChatId chatid)
+	starting_message = SendMessageRequest chatid
 		"Голосование началось - в течении следующих 5 минут вы можете указать игроков, с которыми вы хотели бы поиграть."
-	taking_place_massage = sendMessageRequest (ChatId chatid) "Идёт голосование..."
+		Nothing Nothing Nothing Nothing (Just . candidates_table $ (flip (,) 0) <$> us )
+	taking_place_massage = sendMessageRequest chatid "Идёт голосование..."
 
-	results :: [(User, Int)] -> SendMessageRequest
-	results decisions = sendMessageRequest (ChatId chatid) $
-		"Голосование окончилось, результаты: \n" <>
-		foldr (\x acc -> line x <> acc) "" decisions where
+results :: ChatId -> [(User, Int)] -> SendMessageRequest
+results chatid decisions = SendMessageRequest chatid
+	("Голосование окончилось, результаты: \n" <> foldr (\x acc -> line x <> acc) "" decisions)
+		Nothing Nothing Nothing Nothing (Just $ ReplyKeyboardRemove True Nothing) where
 
-		line :: (User, Int) -> Text
-		line (u, n) = "* " <> user_first_name u <> " "
-			<> (maybe "" id . user_last_name $ u)
-			<> " : " <> (pack . show $ n) <> "\n"
+	line :: (User, Int) -> Text
+	line (u, n) = "* " <> user_first_name u <> " "
+		<> (maybe "" id . user_last_name $ u)
+		<> " : " <> (pack . show $ n) <> "\n"
+
+candidates_table :: [(User, Int)] -> ReplyKeyboard
+candidates_table scores = ReplyInlineKeyboardMarkup $ pure . button <$> scores where
+
+	button :: (User, Int) -> InlineKeyboardButton
+	button (User uid fn ln _ _, n) = InlineKeyboardButton
+		(fn <> maybe "" id ln <> " : " <> (pack . show $ n))
+		Nothing (Just . pack . show $ uid) Nothing Nothing Nothing Nothing
+
+vote = undefined
