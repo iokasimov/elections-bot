6745bd40a9f6d4821acd177619ef5111c1833f5d
Murat Kasimov <iokasimov.m@gmail.com>
2019-03-13 18:03:05 +0300

    Refactor a little bit

--- Mafia/Voting.hs
+++ Mafia/Voting.hs
@@ -33,32 +33,20 @@ initiate settings@(Settings token (ChatId chatid) _ votes) (ChatId chatid') =
 
 	background :: Settings -> IO ()
 	background (Settings token chatid manager votes) = atomically (readTVar votes) >>= \case
-		Just _ -> void $ sendMessage token taking_place_massage manager
-		Nothing -> sendMessage token starting_message manager >>= \case
+		Just _ -> void $ sendMessage token (text_message chatid "Идёт голосование...") manager
+		Nothing -> sendMessage token start_voting_message manager >>= \case
 			Left err -> print err
 			Right res -> do
 				let keyboard_msg_id = message_id . result $ res
 				atomically . writeTVar votes . Just $ (keyboard_msg_id, [])
 				threadDelay 300000000 -- wait for 5 minutes
 				atomically $ modifyTVar' votes (const Nothing)
-				void $ sendMessage token ending_massage manager
+				void $ sendMessage token (text_message chatid "Голосование завершено.") manager
 
-	starting_message, taking_place_massage, ending_massage :: SendMessageRequest
-	starting_message = SendMessageRequest (ChatId chatid)
+	start_voting_message :: SendMessageRequest
+	start_voting_message = SendMessageRequest (ChatId chatid)
 		"Голосование началось - в течении следующих 5 минут вы можете указать игроков, с которыми вы хотели бы поиграть."
 		Nothing Nothing Nothing Nothing (Just . ReplyInlineKeyboardMarkup . candidates_table $ [])
-	taking_place_massage = SendMessageRequest (ChatId chatid) "Идёт голосование..." Nothing Nothing Nothing Nothing Nothing
-	ending_massage = SendMessageRequest (ChatId chatid) "Голосование завершено." Nothing Nothing Nothing Nothing Nothing
-
-	results :: ChatId -> [(User, [User])] -> SendMessageRequest
-	results chatid decisions = SendMessageRequest chatid
-		("Голосование окончилось, результаты: \n" <> foldr (\x acc -> line x <> acc) "" decisions)
-			Nothing Nothing Nothing Nothing (Just $ ReplyKeyboardRemove True Nothing) where
-
-		line :: (User, [User]) -> Text
-		line (u, voters) = "* " <> user_first_name u <> " "
-			<> (maybe "" id . user_last_name $ u)
-			<> " : " <> (pack . show . length $ voters) <> "\n"
 
 vote :: Settings -> Int -> User -> Int -> IO ()
 vote (Settings token group_chatid manager votes) msg_id voter candidate_index = do
@@ -71,21 +59,12 @@ vote (Settings token group_chatid manager votes) msg_id voter candidate_index = 
 participate :: Settings -> ChatId -> User -> IO ()
 participate (Settings token group_chatid manager votes) chatid user = do
 	atomically (readTVar votes) >>= \case
-		Nothing -> void $ sendMessage token not_initiated manager
+		Nothing -> void $ sendMessage token (text_message chatid "Голосование не инициировано в чате мафии.") manager
 		Just (keyboard_msg_id, voters) -> do
 			let new_candidate = (user, []) : voters
 			atomically . writeTVar votes . Just . (,) keyboard_msg_id $ new_candidate
-			void $ sendMessage token taken_part manager
+			void $ sendMessage token (text_message chatid "Теперь вы можете выбирать и быть выбранным.") manager
 			void $ editMessageReplyMarkup token (update_scores new_candidate group_chatid keyboard_msg_id) manager
-	where
-
-	not_initiated :: SendMessageRequest
-	not_initiated = SendMessageRequest chatid "Голосование не инициировано в чате мафии."
-		Nothing Nothing Nothing Nothing Nothing
-
-	taken_part :: SendMessageRequest
-	taken_part = SendMessageRequest chatid "Теперь вы можете выбирать и быть выбранным."
-		Nothing Nothing Nothing Nothing Nothing
 
 candidates_table :: [(User, [User])] -> [[InlineKeyboardButton]]
 candidates_table scores = pure . button <$> zip [0..] scores where
@@ -104,3 +83,7 @@ update_scores :: [(User, [User])] -> ChatId -> Int -> EditMessageReplyMarkupRequ
 update_scores voters group_chatid keyboard_msg_id = EditMessageReplyMarkupRequest
 	(Just group_chatid) (Just keyboard_msg_id ) Nothing . Just . InlineKeyboardMarkup
 		$ candidates_table voters
+
+text_message :: ChatId -> Text -> SendMessageRequest
+text_message chatid txt = SendMessageRequest
+	chatid txt Nothing Nothing Nothing Nothing Nothing
