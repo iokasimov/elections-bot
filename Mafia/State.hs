cd605e16e5add7fb08c32e26fc040b24e28d617c
Murat Kasimov <iokasimov.m@gmail.com>
2019-03-15 15:27:55 +0300

    Refactor a little bit

--- Mafia/State.hs
+++ Mafia/State.hs
@@ -1,4 +1,4 @@
-module Mafia.State (Votes, Scores, consider, nomination) where
+module Mafia.State (Votes, Scores, start, consider, nomination) where
 
 import "base" Data.Eq (Eq ((==)))
 import "base" Data.Foldable (find)
@@ -6,7 +6,7 @@ import "base" Data.Function (const, (.), ($), (&))
 import "base" Data.Functor (fmap)
 import "base" Data.Int (Int)
 import "base" Data.List (delete)
-import "base" Data.Maybe (Maybe, maybe)
+import "base" Data.Maybe (Maybe (Just), maybe)
 import "base" Data.Tuple (fst)
 import "lens" Control.Lens (element, _2, (%~))
 import "telegram-api" Web.Telegram.API.Bot.Data (User (User, user_id))
@@ -18,9 +18,12 @@ type Votes = Maybe (Int, Scores)
 instance Eq User where
 	lu == ru = user_id lu == user_id ru
 
+start :: Int -> Votes
+start keyboard_msg_id = Just (keyboard_msg_id, [])
+
 -- If you already voted for this candidate, your vote will be removed
-consider :: Int -> User -> [(User, [User])] -> [(User, [User])]
-consider candidate_index voter votes = votes & element candidate_index . _2 %~
+consider :: Int -> User -> Votes -> Votes
+consider candidate_index voter = fmap . fmap $ \votes -> votes & element candidate_index . _2 %~
 	(\scores -> maybe (voter : scores) (const $ delete voter scores) . find ((==) (user_id voter) . user_id) $ scores)
 
 -- Application for participation
--- Mafia/Voting.hs
+++ Mafia/Voting.hs
@@ -6,15 +6,15 @@ import "base" Control.Concurrent (threadDelay)
 import "base" Control.Monad (void, (>>=))
 import "base" Data.Int (Int)
 import "base" Data.Either (Either (Left, Right))
-import "base" Data.Function (const, id, (.), ($), (&))
-import "base" Data.Functor (fmap, (<$>))
+import "base" Data.Function (const, flip, id, (.), ($), (&))
+import "base" Data.Functor ((<$>))
 import "base" Data.Traversable (traverse)
+import "base" Data.Tuple (uncurry)
 import "base" Data.List (length, zip)
 import "base" Data.Maybe (Maybe (Just, Nothing), maybe)
 import "base" Data.Semigroup (Semigroup ((<>)))
 import "base" System.IO (IO, print)
 import "base" Text.Show (show)
--- import "lens" Control.Lens (element, _2, (%~))
 import "stm" Control.Concurrent.STM (atomically, modifyTVar', readTVar, writeTVar)
 import "telegram-api" Web.Telegram.API.Bot.API (runClient)
 import "telegram-api" Web.Telegram.API.Bot.API.Edit (deleteMessageM, editMessageReplyMarkup)
@@ -27,7 +27,7 @@ import "telegram-api" Web.Telegram.API.Bot.Responses (Response (..))
 import "text" Data.Text (Text, pack)
 
 import Mafia.Configuration (Settings (Settings))
-import Mafia.State (Scores, consider, nomination)
+import Mafia.State (Scores, start, consider, nomination)
 
 -- Voting takes place in two stages for 5 minutes:
 -- 1) Primaries: someone should initiate it, when putting `/vote` command
@@ -41,34 +41,32 @@ initiate settings@(Settings token chatid manager votes) = atomically (readTVar v
 		Left err -> print err
 		Right res -> do
 			let keyboard_msg_id = message_id . result $ res
-			atomically . writeTVar votes . Just $ (keyboard_msg_id, [])
-			threadDelay 300000000 -- wait for 5 minutes
-			atomically $ modifyTVar' votes (const Nothing)
+			atomically . writeTVar votes $ start keyboard_msg_id
+			threadDelay 300000000 -- Wait all votes for 5 minutes
+			atomically $ modifyTVar' votes (const Nothing) -- Voting is over
 			void $ sendMessage token (text_message chatid "Голосование завершено.") manager
 	where
 
 	start_voting_message :: SendMessageRequest
 	start_voting_message = SendMessageRequest chatid
 		"Голосование началось - в течении следующих 5 минут вы можете указать игроков, с которыми вы хотели бы поиграть."
-		Nothing Nothing Nothing Nothing (Just . ReplyInlineKeyboardMarkup . candidates_table $ [])
+			Nothing Nothing Nothing Nothing (Just . ReplyInlineKeyboardMarkup . candidates_table $ [])
 
 vote :: Settings -> Int -> User -> Int -> IO ()
 vote (Settings token group_chatid manager votes) msg_id voter candidate_index = do
-	atomically $ modifyTVar' votes $ (fmap . fmap) (consider candidate_index voter)
-	atomically (readTVar votes) >>= \case
-		Nothing -> print "Very strange situation"
-		Just (keyboard_msg_id, voters) -> void $ editMessageReplyMarkup token
-			(update_keyboard voters group_chatid keyboard_msg_id) manager
+	atomically $ modifyTVar' votes (consider candidate_index voter)
+	atomically (readTVar votes) >>= void . traverse
+		(flip (editMessageReplyMarkup token) manager . uncurry (update_keyboard group_chatid))
 
 participate :: Settings -> Int -> User -> IO ()
-participate (Settings token group_chatid manager votes) msgid user = do
+participate settings@(Settings token group_chatid manager votes) msgid user = do
 	atomically (modifyTVar' votes (nomination user) *> readTVar votes) >>=
 		void . traverse update_keyboard_and_delete_message where
 
 	update_keyboard_and_delete_message :: (Int, Scores) -> IO ()
 	update_keyboard_and_delete_message (keyboard_msg_id, scores) = do
-		void $ editMessageReplyMarkup token (update_keyboard scores group_chatid keyboard_msg_id) manager
-		void $ runClient (deleteMessageM $ DeleteMessageRequest group_chatid msgid) token manager
+		void $ editMessageReplyMarkup token (update_keyboard group_chatid keyboard_msg_id scores) manager
+		delete_message settings msgid
 
 candidates_table :: [(User, [User])] -> [[InlineKeyboardButton]]
 candidates_table scores = pure . button <$> zip [0..] scores where
@@ -78,11 +76,15 @@ candidates_table scores = pure . button <$> zip [0..] scores where
 		(fn <> " " <> maybe "" id ln <> " : " <> (pack . show . length $ n))
 		Nothing (Just . pack . show $ idx) Nothing Nothing Nothing Nothing
 
-update_keyboard :: [(User, [User])] -> ChatId -> Int -> EditMessageReplyMarkupRequest
-update_keyboard scores group_chatid keyboard_msg_id = EditMessageReplyMarkupRequest
+update_keyboard :: ChatId -> Int -> [(User, [User])] -> EditMessageReplyMarkupRequest
+update_keyboard group_chatid keyboard_msg_id scores = EditMessageReplyMarkupRequest
 	(Just group_chatid) (Just keyboard_msg_id ) Nothing . Just . InlineKeyboardMarkup
 		$ candidates_table scores
 
 text_message :: ChatId -> Text -> SendMessageRequest
 text_message chatid txt = SendMessageRequest
 	chatid txt Nothing Nothing Nothing Nothing Nothing
+
+delete_message :: Settings -> Int -> IO ()
+delete_message (Settings token group_chatid manager _) msgid = void $ runClient
+	(deleteMessageM $ DeleteMessageRequest group_chatid msgid) token manager
