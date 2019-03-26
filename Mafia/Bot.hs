605fad4b841f6ce14373e5d6a1f36e56bf70e523
Murat Kasimov <iokasimov.m@gmail.com>
2019-03-26 18:15:56 +0300

    Testing post the message and edit its keyboard with timeout

--- Mafia/Bot.hs
+++ Mafia/Bot.hs
@@ -12,13 +12,20 @@ import "base" System.IO (IO, print)
 import "base" Text.Read (readMaybe)
 import "servant-server" Servant (Capture, ReqBody, Proxy (Proxy)
 	, Server, JSON, Get, Post, type (:>), serve, err403, throwError)
-import "telega" Network.Telegram.API.Bot.Capacity.Postable (Postable (post), Initial)
-import "telega" Network.Telegram.API.Bot.Capacity.Purgeable (Purgeable (purge), Marking)
-import "telega" Network.Telegram.API.Bot.Object.Chat (Chat (Group))
-import "telega" Network.Telegram.API.Bot.Object.Keyboard (Keyboard (Inline), Button (Button), Pressed (Open))
-import "telega" Network.Telegram.API.Bot.Object.Message (Message (Message), Initial, Marking)
-import "telega" Network.Telegram.API.Bot.Object.Update (Update (Incoming))
-import "telega" Network.Telegram.API.Bot (Telegram, Token (Token), telegram)
+import "telega" Network.Telegram.API.Bot.Capacity.Editable
+	(Editable (edit), Substitution)
+import "telega" Network.Telegram.API.Bot.Capacity.Postable
+	(Postable (post), Initial)
+import "telega" Network.Telegram.API.Bot.Capacity.Purgeable
+	(Purgeable (purge), Marking)
+import "telega" Network.Telegram.API.Bot.Object.Keyboard
+	(Keyboard (Inline), Button (Button), Pressed (Open), Substitution)
+import "telega" Network.Telegram.API.Bot.Object.Message
+	(Message (Message), Initial, Marking)
+import "telega" Network.Telegram.API.Bot.Object.Update
+	(Update (Incoming))
+import "telega" Network.Telegram.API.Bot
+	(Telegram, Token (Token), telegram)
 import "transformers" Control.Monad.Trans.Class (lift)
 import "warp" Network.Wai.Handler.Warp (run)
 
@@ -39,11 +46,15 @@ test_inline_keyboard = Inline . pure $
 	Button "click me" (Open "http://www.nooooooooooooooo.com") :
 	Button "do not click me" (Open "http://www.nooooooooooooooo.com") : []
 
+test_inline_keyboard_replacement :: Keyboard
+test_inline_keyboard_replacement = Inline . pure $
+	Button "don't touch me..." (Open "http://www.nooooooooooooooo.com") : []
+
 main = do
-	Settings token chatid session _ <- settings
-	-- Post and purge the message with timeout
-	telegram session token chatid $ do
-		msg <- post @Message (chatid, "hello there", Just test_inline_keyboard)
-		let Message msg_id (Group chat_id _) _ _ = msg
-		lift . lift $ print msg *> print "wait..." *> threadDelay 5000000
-		purge @Message (chat_id, msg_id)
+	Settings token chat_id session _ <- settings
+	-- Post the message and edit keyboard with timeout
+	telegram session token chat_id $ do
+		msg <- post @Message (chat_id, "hello there", Just test_inline_keyboard)
+		let Message msg_id _ _ _ = msg
+		lift . lift $ print msg *> print "wait..." *> threadDelay 30000000
+		edit @Keyboard (chat_id, msg_id, test_inline_keyboard_replacement)
