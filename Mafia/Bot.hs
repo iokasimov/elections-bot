e6748ade75af0efe21af2d592d6667fecb576d8b
Murat Kasimov <iokasimov.m@gmail.com>
2019-03-26 14:49:30 +0300

    Testing post and purge the message with timeout

--- Mafia/Bot.hs
+++ Mafia/Bot.hs
@@ -1,7 +1,8 @@
 module Main where
 
 import "async" Control.Concurrent.Async (async)
-import "base" Control.Applicative (pure)
+import "base" Control.Applicative (pure, (*>))
+import "base" Control.Concurrent (threadDelay)
 import "base" Control.Monad (void, (>>=))
 import "base" Control.Monad.IO.Class (liftIO)
 import "base" Data.Eq (Eq ((==), (/=)))
@@ -11,14 +12,15 @@ import "base" System.IO (IO, print)
 import "base" Text.Read (readMaybe)
 import "servant-server" Servant (Capture, ReqBody, Proxy (Proxy)
 	, Server, JSON, Get, Post, type (:>), serve, err403, throwError)
-import "telega" Network.Telegram.API.Bot.Capacity.Postable (Postable (post))
+import "telega" Network.Telegram.API.Bot.Capacity.Postable (Postable (post), Initial)
+import "telega" Network.Telegram.API.Bot.Capacity.Purgeable (Purgeable (purge), Marking)
+import "telega" Network.Telegram.API.Bot.Object.Chat (Chat (Group))
 import "telega" Network.Telegram.API.Bot.Object.Keyboard (Keyboard (Inline), Button (Button), Pressed (Open))
-import "telega" Network.Telegram.API.Bot.Object.Message (Payload (Msg))
+import "telega" Network.Telegram.API.Bot.Object.Message (Message (Message), Initial, Marking)
 import "telega" Network.Telegram.API.Bot.Object.Update (Update (Incoming))
 import "telega" Network.Telegram.API.Bot (Telegram, Token (Token), telegram)
+import "transformers" Control.Monad.Trans.Class (lift)
 import "warp" Network.Wai.Handler.Warp (run)
-
-import qualified Data.Text as T (take, unpack)
 
 import Mafia.Configuration (Settings (Settings), settings)
 
@@ -39,4 +41,9 @@ test_inline_keyboard = Inline . pure $
 
 main = do
 	Settings token chatid session _ <- settings
-	telegram session token chatid (post . Msg chatid "hello there" $ Just test_inline_keyboard) >>= print
+	-- Post and purge the message with timeout
+	telegram session token chatid $ do
+		msg <- post @Message (chatid, "hello there", Just test_inline_keyboard)
+		let Message msg_id (Group chat_id _) _ _ = msg
+		lift . lift $ print msg *> print "wait..." *> threadDelay 5000000
+		purge @Message (chat_id, msg_id)
