a8b79c30449bc158eda84e328114a5aa7f8142cb
Murat Kasimov <iokasimov.m@gmail.com>
2019-03-30 15:48:46 +0300

    Put back voting implementaion from master branch

--- Mafia/Bot.hs
+++ Mafia/Bot.hs
@@ -7,10 +7,10 @@ import "base" Control.Monad (void, (>>=))
 import "base" Control.Monad.IO.Class (liftIO)
 import "base" Data.Eq (Eq ((==), (/=)))
 import "base" Data.Foldable (find, length)
-import "base" Data.Function (const, id, (.), ($))
+import "base" Data.Function (const, id, (.), ($), (&))
 import "base" Data.Functor (fmap, (<$>))
 import "base" Data.Int (Int, Int64)
-import "base" Data.List (zip)
+import "base" Data.List (delete, zip)
 import "base" Data.Maybe (Maybe (Just, Nothing), maybe)
 import "base" Data.Semigroup ((<>))
 import "base" Data.Tuple (fst)
@@ -18,6 +18,7 @@ import "base" Prelude (negate)
 import "base" System.IO (IO, print)
 import "base" Text.Read (readMaybe)
 import "base" Text.Show (show)
+import "lens" Control.Lens (element, _2, (%~))
 import "optparse-applicative" Options.Applicative (Parser, execParser, argument, auto, info, fullDesc, metavar, str)
 import "servant-server" Servant (Capture, ReqBody, Proxy (Proxy), Server, JSON
 	, Get, Post, FromHttpApiData, ToHttpApiData, type (:>), serve, err403, throwError)
@@ -37,7 +38,7 @@ import "transformers" Control.Monad.Trans.Class (lift)
 import "warp" Network.Wai.Handler.Warp (run)
 import "wreq" Network.Wreq.Session (Session, newAPISession)
 
-import qualified "text" Data.Text as T (Text, pack)
+import qualified "text" Data.Text as T (Text, pack, unpack)
 import qualified "text" Data.Text.IO as T (putStrLn)
 
 type Scores = [(From, [From])]
@@ -48,6 +49,11 @@ type Votes = Maybe (Int, Scores)
 nomination :: From -> Votes -> Votes
 nomination user = fmap . fmap $ \us ->
 	maybe ((user, []) : us) (const us) . find ((==) user . fst) $ us
+
+-- If you already voted for this candidate, your vote will be removed
+consider :: Int -> From -> Scores -> Scores
+consider candidate_index voter votes = votes & element candidate_index . _2 %~
+	(\scores -> maybe (voter : scores) (const $ delete voter scores) . find (== voter) $ scores)
 
 initiate :: Telegram (Int64, TVar Votes) ()
 initiate = ask' >>= \(chat_id, votes) ->
@@ -67,12 +73,20 @@ participate from = ask' >>= \(chat_id, votes) ->
 	(lift . lift . atomically $ modifyTVar' votes (nomination from) *> readTVar votes) >>= \case
 		Nothing -> void $ post @Message (chat_id, "Голосование не иницировано...", Nothing)
 		Just (keyboard_msg_id, scores) -> void $ edit @Keyboard
-			(chat_id, keyboard_msg_id, Inline $ pure . button <$> zip [0..] scores) where
+			(chat_id, keyboard_msg_id, Inline $ pure . button <$> zip [0..] scores)
 
-			button :: (Int, (From, [From])) -> Button
-			button (idx, (User uid _ fn ln _, n)) = Button
-				(fn <> " " <> maybe "" id ln <> " : " <> (T.pack . show . length $ n))
-				(Callback . T.pack . show $ idx)
+button :: (Int, (From, [From])) -> Button
+button (idx, (User uid _ fn ln _, n)) = Button
+	(fn <> " " <> maybe "" id ln <> " : " <> (T.pack . show . length $ n))
+	(Callback . T.pack . show $ idx)
+
+vote :: From -> Int -> Telegram (Int64, TVar Votes) ()
+vote from cnd_idx = ask' >>= \(chat_id, votes) -> do
+	let considering = modifyTVar' votes (fmap . fmap $ consider cnd_idx from) *> readTVar votes
+	(lift . lift . atomically $ considering) >>= \case
+		Nothing -> lift . lift $ print "Very strange situation"
+		Just (keyboard_msg_id, scores) -> void $ edit @Keyboard
+			(chat_id, keyboard_msg_id, Inline $ pure . button <$> zip [0..] scores)
 
 type API = "webhook" :> Capture "secret" Token :> ReqBody '[JSON] Update :> Post '[JSON] ()
 
@@ -84,10 +98,13 @@ server session token chat_id votes secret update = if secret /= token then throw
 	else liftIO . void . telegram session token (chat_id, votes) $ webhook update
 
 webhook :: Update -> Telegram (Int64, TVar Votes) ()
-webhook (Query _ u) = lift . lift $ print u
 webhook (Incoming _ (Textual _ _ _ txt)) = lift . lift $ T.putStrLn txt
-webhook (Incoming _ (Command msg_id (Group chat_id _) _ "vote")) = initiate *> purge @Message (chat_id, msg_id)
-webhook (Incoming _ (Command msg_id (Group chat_id _) from "participate")) = participate from *> purge @Message (chat_id, msg_id)
+webhook (Query _ (Datatext from msg txt)) =
+	maybe (pure ()) (void . vote from) $ readMaybe (T.unpack txt)
+webhook (Incoming _ (Command msg_id (Group chat_id _) _ "initiate")) =
+	initiate *> purge @Message (chat_id, msg_id)
+webhook (Incoming _ (Command msg_id (Group chat_id _) from "participate")) =
+	participate from *> purge @Message (chat_id, msg_id)
 webhook _ = lift . lift $ print "Undefined update"
 
 test_inline_keyboard :: Keyboard
