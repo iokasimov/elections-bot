88eff5c34d29b9002070406898111974c1f9a905
Murat Kasimov <iokasimov.m@gmail.com>
2021-07-03 12:15:16 +0300

    Add logger middleware to test incoming requests

--- Network/API/Telegram/Bot/Main.hs
+++ Network/API/Telegram/Bot/Main.hs
@@ -4,8 +4,9 @@ import "base" Control.Monad ((>>=))
 import "base" Data.Function ((.))
 import "servant-server" Servant (Proxy (Proxy), serve)
 import "warp" Network.Wai.Handler.Warp (run)
+import "wai-extra" Network.Wai.Middleware.RequestLogger (logStdoutDev)
 
 import Network.API.Telegram.Bot.Elections.Configuration (settings)
 import Network.API.Telegram.Bot.Elections.Server (API, server)
 
-main = settings >>= run 8080 . serve (Proxy :: Proxy API) . server
+main = settings >>= run 8080 . logStdoutDev . serve (Proxy :: Proxy API) . server
